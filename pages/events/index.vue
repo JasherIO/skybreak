7e8f6f41df4ed020a85583df2c8ee6b381186294
Willis <3475947+JasherIO@users.noreply.github.com>
2020-05-25 16:57:43 -0500

    Page stubs for navbar navigation

---
+++ pages/events/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <h1 class="text-2xl">Events</h1>
+    <!-- <button
+      type="button"
+      class="bg-teal-700 hover:bg-teal-500 text-white font-bold py-2 px-4 rounded"
+    >
+      <nuxt-link :to="{ path: 'events', query: { region: 'North America' } }"
+        >North America</nuxt-link
+      >
+    </button> -->
+  </div>
+</template>
